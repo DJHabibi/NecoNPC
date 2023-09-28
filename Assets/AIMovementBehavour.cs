b404c1db3adb5cd823d15e05d86f8d8a58d161eb
DJHabibi <dariovanderlaangx@gmail.com>
2023-09-28 16:39:52 +0200

    NPC has walking animation and the wait a random time everytime they reach destination

--- Assets/AIMovementBehavour.cs
+++ Assets/AIMovementBehavour.cs
@@ -9,13 +9,17 @@ public class AIMovementBehavour : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
     public float maxDistance;
+    public Animator npcAnimator;
     private void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
-        SetRandomDestination();
+       
         StartCoroutine(RandomWalk());
     }
-
+    public void Update()
+    {
+        CheckMoving();
+    }
     private void SetRandomDestination()
     {
         Vector3 randomNavMeshPoint = GetRandomNavMeshPoint();
@@ -37,13 +41,27 @@ public class AIMovementBehavour : MonoBehaviour
     }
     private IEnumerator RandomWalk()
     {
+        SetRandomDestination();
         while (true)
         {
             if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
             {
                 SetRandomDestination();
             }
-            yield return new WaitForSeconds(10.0f);
+
+            // Check if the agent's velocity magnitude is greater than a small threshold.     
+            yield return new WaitForSeconds(Random.Range(5,20)); // Yielding once per frame.
+        }
+    }
+    private void CheckMoving()
+    {
+        if (navMeshAgent.velocity.magnitude > 0.1f)
+        {
+            npcAnimator.SetBool("Walking", true);
+        }
+        else
+        {
+            npcAnimator.SetBool("Walking", false);
         }
     }
 }
